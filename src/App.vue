78ea92e9ed816a52e4aafe71d2911a1d7d3f1d3a
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-02-23 22:05:30 -0400

    ugfg

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer"
+        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        permanent
+        theme="dark"
+      >
+        <v-list nav>
+          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+      
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+import router from './router'
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+let app = createApp(App).use(vuetify, router)
+
+app.mount('#app')
