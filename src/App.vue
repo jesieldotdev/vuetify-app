1f00d3ea89615055ca1a82e30e0099529af3ac50
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-02-23 22:59:52 -0400

    sdfsdf

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,8 @@
         theme="dark"
       >
         <v-list nav>
-          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+          <v-list-item prepend-icon="mdi-email" title="About" value="inbox"></v-list-item>
           <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
-          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
@@ -21,6 +20,8 @@
 
     <v-main>
       <!--  -->
+      <RouterView />
+      <RouterLink to='/about'>sdssss</RouterLink>
     </v-main>
   </v-app>
 </template>
