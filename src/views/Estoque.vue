746357cf39b51dca4a8afe44a6d5aa7f019b5f27
Jesiel364 <82860253+jesiel364@users.noreply.github.com>
2023-03-04 13:51:30 -0400

    page estoque and card prod

--- src/views/Estoque.vue
+++ src/views/Estoque.vue
@@ -1,55 +1,105 @@
 <template>
- 
- <!-- <ProdutosComp /> -->
-  <v-list lines="two">
-      <div class="d-flex justify-space-around align-center flex-column flex-sm-row">
-<v-btn
-      variant="flat"
-      to="/addproduto"
-      color="info"
-      
-    >Adicionar produto 
-      
-    </v-btn>
-    </div>
-    <v-list-subheader inset>Locações</v-list-subheader>
-  
-    <v-list-item v-for="loc in locacoes" :key="loc.title" :title="loc.title" :subtitle="loc.desc">
-      <template v-slot:prepend>
-        <v-avatar color="blue">
-          <v-icon color="white">mdi-package</v-icon>
-        </v-avatar>
-      </template>
+  <v-btn class="mx-auto mt-3 ms-3" id="btn" variant="flat" to="/addproduto" color="info">Adicionar produto
 
-      <template v-slot:append>
-        <v-btn color="grey-lighten-1" icon="mdi-information" variant="text"></v-btn>
-      </template>
-    </v-list-item>
+  </v-btn>
 
-    <v-divider inset></v-divider>
+  <v-container >
+    <!-- <v-row> -->
+      <v-row v-for="item in items" :key="items" >
+        <v-card style='width: 100%;'  class='mx-2 mb-2 mt-2' >
 
 
-  </v-list>
+          <v-card-title>{{item.cod}}</v-card-title>
+          <v-card-text>{{ item.desc }}</v-card-text>
 
+          <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          Estoque
+        </th>
+        <th class="text-left">
+          A comprar
+        </th>
+        <th class="text-left">
+          Vlr. unit.
+        </th>
+        <th class="text-left">
+          Marca
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        
+      >
+        <td class="font-weight-bold">{{ item.qtde }}</td>
+        <td>1</td>
+        <td><p  class="font-weight-bold">R${{item.preco}}</p></td>
+        <td>{{item.marca}}</td>
+      </tr>
+    </tbody>
+          </v-table>
+          <!-- <v-divider></v-divider> -->
+
+          <v-card-actions>
+            <v-row justify="space-between" class="text-center">
+      <v-col>
+        <v-btn
+          icon="mdi-pencil"
+          color="primary"
+        ></v-btn>
+      </v-col>
+
+      <v-col>
+        <v-btn
+          icon="mdi-barcode-scan"
+          color="secondary"
+        ></v-btn>
+      </v-col>
+
+      <v-col>
+        <v-btn
+          icon="mdi-cached"
+          color="info"
+        ></v-btn>
+      </v-col>
+
+      <v-col>
+        <v-btn
+          icon="mdi-menu-down-outline"
+          color="light"
+        ></v-btn>
+      </v-col>
+    </v-row>
+          </v-card-actions>
+        </v-card>
+      <!-- </v-col> -->
+    </v-row>
+  </v-container>
 </template>
 
-
+<style scoped>
+  #btn{
+    width: 200px;
+ 
+    
+  }
+</style>
 
 <script>
-// import ProdutosComp from '../components/ProdutosComp.vue'
 export default {
   data: () => ({
-  
-    locacoes: [{
-      title: '01A001',
-      desc: '01A001'
-    },
-    {
-      title: '01A002',
-      desc: '01A002'
-    }
-  ],
+    items: [{
+      cod: '336256AM',
+      desc: 'fghfgfh',
+      preco: '456',
+      marca: 'hjk',
+      qtde: 23,
+      qtde_und: 1,
+
+      
+    },]
   })
 }
-
 </script>
---
+++ src/views/Locacoes.vue
@@ -0,0 +1,21 @@
+<template>
+      <v-list lines="two">
+    <v-list-subheader inset>Locações</v-list-subheader>
+
+    <v-list-item v-for="loc in locacoes" :key="loc.title" :title="loc.title" :subtitle="loc.desc">
+      <template v-slot:prepend>
+        <v-avatar color="blue">
+          <v-icon color="white">mdi-package</v-icon>
+        </v-avatar>
+      </template>
+
+      <template v-slot:append>
+        <v-btn color="grey-lighten-1" icon="mdi-information" variant="text"></v-btn>
+      </template>
+    </v-list-item>
+
+    <v-divider inset></v-divider>
+
+
+  </v-list>
+</template>
